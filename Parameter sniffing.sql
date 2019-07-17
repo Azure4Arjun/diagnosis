5748056a29a2c12d87bfa340543befc5e756f90c
rahulbelokar <49721093+rahulbelokar@users.noreply.github.com>
2019-07-17 12:32:27 +0530

    Update Parameter sniffing.sql

--- Parameter sniffing.sql
+++ Parameter sniffing.sql
@@ -1,3 +1,41 @@
+CREATE PROC dbo.AddressByCity @City NVARCHAR(30)
+AS
+-- This allows me to bypass parameter sniffing
+DECLARE @LocalCity NVARCHAR(30) = @City;
+
+SELECT  a.AddressID,
+        a.AddressLine1,
+        AddressLine2,
+        a.City,
+        sp.[Name] AS StateProvinceName,
+        a.PostalCode
+FROM    Person.Address AS a
+JOIN    Person.StateProvince AS sp
+        ON a.StateProvinceID = sp.StateProvinceID
+WHERE   a.City = @LocalCity;
+GO
+
+
+---method 2
+
+ALTER PROC dbo.AddressByCity @City NVARCHAR(30)
+AS
+SELECT  a.AddressID,
+        a.AddressLine1,
+        AddressLine2,
+        a.City,
+        sp.[Name] AS StateProvinceName,
+        a.PostalCode
+FROM    Person.Address AS a
+JOIN    Person.StateProvince AS sp
+        ON a.StateProvinceID = sp.StateProvinceID
+WHERE   a.City = @City
+OPTION (OPTIMIZE FOR (@City = 'Mentor'));
+
+exec dbo.addressbycity @city = 'London';
+
+
+--method 1
 CREATE PROCEDURE Customer_Search
  @FirstName varchar(100),
  @LastName varchar(100)
